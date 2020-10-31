c4afcdafbed016bf8cdcc503402c7581aa432b22
alexesca <alexescamore@gmail.com>
2020-10-31 07:24:19 -0600

    Added Path and Delete methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -19,4 +19,21 @@ export class CoffeesController {
     create(@Body() body) {
         return body
     }
+
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body() body
+    ) {
+        return `This action updates #${id} cofee`
+    }
+
+    @Delete(':id')
+    delete(
+        @Param('id') id: string,
+    ) {
+        return `This action deletes #${id} cofee`
+    }
+
+
 }
