9aef29487dd555b4cd80320d6bd98bc9cec8e823
alexesca <alexescamore@gmail.com>
2020-11-21 11:03:39 -0700

    Added loggin middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
     imports: [
@@ -14,4 +15,13 @@ import { ApiKeyGuard } from './guards/api-key.guard';
         }
     ]
 })
-export class CommonModule { }
+export class CommonModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(
+            LoggingMiddleware
+        )
+            .forRoutes(
+                '*'
+            )
+    }
+}
